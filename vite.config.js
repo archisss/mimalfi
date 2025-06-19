c937ad0965ab1e004e58ff8745b1e53a459f7700
José Carlos Anaya <j053carlosanach@gmail.com>
2025-06-19 16:41:47 -0600

    vite config update to deploy

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,12 @@ export default defineConfig({
     server: {
         cors: true,
     },
+    build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: process.env.NODE_ENV === 'production' ? process.env.APP_URL + '/' : '/',
 });
