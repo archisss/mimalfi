8f0446a2b3212ab2d6117da8a87138042674219b
José Carlos Anaya <j053carlosanach@gmail.com>
2025-06-19 17:02:12 -0600

    vite new config update to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+
+// Obtén la URL de producción del archivo .env
+const isProduction = process.env.NODE_ENV === 'production';
+const baseURL = isProduction ? process.env.VITE_APP_URL : '/';
 
 export default defineConfig({
     plugins: [
@@ -16,11 +18,11 @@ export default defineConfig({
         cors: true,
     },
     build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
     },
-  },
-  base: process.env.NODE_ENV === 'production' ? process.env.APP_URL + '/' : '/',
+    base: baseURL, // Cambiado para que base use la URL adecuada
 });
