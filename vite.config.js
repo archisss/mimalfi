d58b04d46658db7b9923a40f6398cd45a54d2618
José Carlos Anaya <j053carlosanach@gmail.com>
2025-06-19 18:52:00 -0600

    delete unecesarry lines for VITE

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-// Obtén la URL de producción del archivo .env
-const isProduction = process.env.NODE_ENV === 'production';
-const baseURL = isProduction ? process.env.VITE_APP_URL : '/';
-
 export default defineConfig({
     plugins: [
         laravel({
