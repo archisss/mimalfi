b4822803a6961cd0a4b610b2666c9be8e3971ce6
José Carlos Anaya <j053carlosanach@gmail.com>
2025-06-28 16:14:45 -0600

    loanTypeSeed fix

--- database/seeders/LoanTypesTableSeeder.php
+++ database/seeders/LoanTypesTableSeeder.php
@@ -15,30 +15,28 @@ class LoanTypesTableSeeder extends Seeder
     public function run(): void
     {
          LoanType::updateOrCreate([
-            [
                 'name' => 'Semana10',
                 'calendar_days' => 7,
                 'payments_total' => 10,
                 'porcentage' => 25.00,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-            ],
-            [
+            ]);
+            LoanType::updateOrCreate([
                 'name' => 'Semana15',
                 'calendar_days' => 7,
                 'payments_total' => 15,
                 'porcentage' => 20,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-            ],
-            [
+            ]);
+            LoanType::updateOrCreate([
                 'name' => 'Mensual10',
                 'calendar_days' => 30,
                 'payments_total' => 10,
                 'porcentage' => 15,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-            ],
-        ]);
+            ]);       
     }
 }
